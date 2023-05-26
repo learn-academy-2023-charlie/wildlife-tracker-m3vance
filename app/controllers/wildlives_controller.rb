900e97385f8bf766f316f6cf3bfcc62eaa56ef95
m3vance <m3vance@gmail.com>
2023-05-26 09:36:12 -0700

    story 1 complete

--- app/controllers/wildlives_controller.rb
+++ app/controllers/wildlives_controller.rb
@@ -14,6 +14,25 @@ class WildlivesController < ApplicationController
     end
   end
 
+  def update
+    wildlife = Wildlife.find(params[:id])
+    wildlife.update(wildlife_params)
+    if wildlife.valid?
+      render json: wildlife
+    else
+      render json: wildlife.errors
+    end
+  end
+
+  def destroy
+    wildlife = Wildlife.find(params[:id])
+    if wildlife.destroy
+      render json: wildlife
+    else
+      render json: wildlife.errors
+    end
+  end
+
   private
   def wildlife_params
     params.require(:wildlife).permit(:common_name, :binomial_name)
